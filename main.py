f89555b597bbced7a71e197e59df38cdb301c156
Rik Ghosh <rikghosh487@gmail.com>
2022-08-11 17:12:06 -0500

    main file for the user to work with

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+'''
+    Photometric Approximator of Stellar Metallicity (c) by Rik Ghosh, Soham Saha
+
+    Photometric Approximator of Stellar Metallicity is licensed under a
+    Creative Commons Attribution 4.0 International License.
+
+    You should have received a copy of the license along with this work.
+    If not, see https://creativecommons.org/licenses/by/4.0
+'''
+
+# third party imports
+import pandas as pd
+
+# custom library imports
+from modules.model import model_predict, train, score_eval
+
+if __name__ == '__main__':
+    ''' 
+    ------------ Implementation left to the user ------------
+    
+    Basic Instructions:
+        1. Train the Model (and save it)
+        2. Evaluate Model Accuracy. If not satisfied, shuffle data and then train again
+        3. Copy the data file (.csv) into the parent directory
+        4. Obtain a pandas dataframe from the data
+        5. Run model_predict()
+
+    '''
+
+    pass
